336e11007f83b67a65bf9b618c3d76499768a5d3
PedroStonks <pedrohenriquemigue76@gmail.com>
2025-08-04 11:55:20 -0300

    CrudTDS03

---
+++ Uc13_ReactNative/CrudTDS03/src/firebaseConnection.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSbRJAaiK4yFc0gCoQ5VFvkStoWGQGa8I",
+  authDomain: "tds03-bb53b.firebaseapp.com",
+  projectId: "tds03-bb53b",
+  storageBucket: "tds03-bb53b.firebasestorage.app",
+  messagingSenderId: "447237225936",
+  appId: "1:447237225936:web:35719a2b23558985b14dd5"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore(app);
+
+export {db};
